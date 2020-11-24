127a8a1a029e6e5a701705770abe637ee317cf6a
xunixianshigo <xunixianshigo@gmail.com>
2020-11-24 16:36:03 +0800

    docker

--- Pallas/pallas-server/pallas-cloud-server/pallas-cloud-gateway/src/main/bin/start.sh
+++ Pallas/pallas-server/pallas-cloud-server/pallas-cloud-gateway/src/main/bin/start.sh
@@ -29,6 +29,14 @@ STDOUT_FILE=$LOGS_DIR/stdout.log
 LIB_DIR=$DEPLOY_DIR/lib
 LIB_JARS=`ls $LIB_DIR|grep .jar|awk '{print "'$LIB_DIR'/"$0}'|tr "\n" ":"`
 
+JAVA_ARGS=""
+if [ "$1" = "consul" ]; then
+    JAVA_ARGS=" -Dspring.cloud.consul.host=$2 "
+fi
+if [ "$1" = "token" ]; then
+    JAVA_ARGS=" -Dspring.cloud.consul.host=$2 -Dspring.cloud.consul.discovery.acl-token=$3 "
+fi
+
 JAVA_OPTS=" -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true "
 JAVA_DEBUG_OPTS=""
 if [ "$1" = "debug" ]; then
@@ -48,7 +56,7 @@ fi
 
 echo -e "Starting the $SERVER_NAME ...\c"
 #优先从同级config目录下获取配置文件信息
-nohup java $JAVA_OPTS $JAVA_MEM_OPTS $JAVA_DEBUG_OPTS $JAVA_JMX_OPTS -jar $DEPLOY_DIR/$JAR_NAME > /dev/null 2>&1 &
+nohup java $JAVA_OPTS $JAVA_ARGS $JAVA_MEM_OPTS $JAVA_DEBUG_OPTS $JAVA_JMX_OPTS -jar $DEPLOY_DIR/$JAR_NAME > /dev/null 2>&1 &
 
 echo "$SERVER_NAME 启动成功！"
 PIDS=`ps -f | grep java | grep "$DEPLOY_DIR" | awk '{print $2}'`
